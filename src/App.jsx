cf04a8b54e301f145fb37d9e36d90414ca45d398
Mohammad Rafi Safidrawan <safidrawan@gmail.com>
2025-02-27 20:40:02 +0430

    API Connected to get exchange rates.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Card from './components/Card'
 import BgChanger from './components/BgChanger'
 import PassGenerator from './components/PassGenerator'
 import CurrencyConverter from './components/CurrencyConverter'
-import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 
 function App() {
-
-  // Currency Converter related codes
-
-  const [amount , setAmount] = useState(0)
-  const [from , setFrom] = useState('usd')
-  const [to, setTo] = useState('inr')
-  const [convertedAmount, setConvertedAmount] = useState(0)
-
-  const currencyInfo = useCurrencyInfo(from)
-  const options = Object.keys(currencyInfo)
-  const convert = ()=>{
-    setConvertedAmount(amount * currencyInfo[to])
-  }
-
-
   return (
     <>
     <CurrencyConverter />
--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -1,11 +1,60 @@
-import React from "react";
-import InputBox from "./InputBox";
+import React,{useState} from "react";
+import {InputBox} from "./index";
+import useCurrencyInfo from '/src/hooks/useCurrencyInfo'
+
 
 function CurrencyConverter() {
+
+  const [amount , setAmount] = useState(0)
+  const [from , setFrom] = useState('usd')
+  const [to, setTo] = useState('inr')
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+  const options = Object.keys(currencyInfo)
+  const convert = ()=>{
+    setConvertedAmount(amount * currencyInfo[to])
+  }
+
+  const swap = ()=>{
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
   return (
-    <div className="flex flex-wrap w-full h-screen bg-cover bg-no-repeat justify-center items-center" style={{backgroundImage:`url(/src/assets/moneybg.webp)`}}>
-      <InputBox />
-    </div>
+    <>
+      <div
+        className="flex flex-wrap w-full h-screen bg-cover bg-no-repeat justify-center items-center"
+        style={{ backgroundImage: `url(/src/assets/moneybg.webp)` }}
+      >
+        <InputBox />
+      </div>
+      <div className="w-full">
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+        <form onSubmit={(e)=>{
+          e.preventDefault
+
+        }}></form>
+
+        <div className="w-full mb-1">
+          <InputBox
+          label={from}
+          amount={amount}
+          currencyOptions={options}
+          onCurrencyChange={(currency)=>setFrom(currency)}
+          onAmountChange={(amount)=>setAmount(amount)}
+          selectedCurrency={from}
+          
+          />
+        </div>
+        <div className="relative w-full h-0.5">
+          <button className="absolute">Swap</button>
+        </div>
+        
+        </div>
+      </div>
+    </>
   );
 }
 
