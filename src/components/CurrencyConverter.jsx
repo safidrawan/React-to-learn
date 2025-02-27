9faf52b17c6286b6d9b66d79568b2ba5b0fbcba2
Mohammad Rafi Safidrawan <safidrawan@gmail.com>
2025-02-27 21:30:05 +0430

    Converstion working now

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -1,57 +1,71 @@
-import React,{useState} from "react";
-import {InputBox} from "./index";
-import useCurrencyInfo from '/src/hooks/useCurrencyInfo'
-
+import React, { useState } from "react";
+import { InputBox } from "./index";
+import useCurrencyInfo from "/src/hooks/useCurrencyInfo";
 
 function CurrencyConverter() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
 
-  const [amount , setAmount] = useState(0)
-  const [from , setFrom] = useState('usd')
-  const [to, setTo] = useState('inr')
-  const [convertedAmount, setConvertedAmount] = useState(0)
+  const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo);
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
 
-  const currencyInfo = useCurrencyInfo(from)
-  const options = Object.keys(currencyInfo)
-  const convert = ()=>{
-    setConvertedAmount(amount * currencyInfo[to])
-  }
-
-  const swap = ()=>{
-    setFrom(to)
-    setTo(from)
-    setConvertedAmount(amount)
-    setAmount(convertedAmount)
-  }
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  };
   return (
     <>
       <div
         className="flex flex-wrap w-full h-screen bg-cover bg-no-repeat justify-center items-center"
         style={{ backgroundImage: `url(/src/assets/moneybg.webp)` }}
       >
-        <InputBox />
-      </div>
-      <div className="w-full">
-        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
-        <form onSubmit={(e)=>{
-          e.preventDefault
-
-        }}></form>
-
-        <div className="w-full mb-1">
-          <InputBox
-          label={from}
-          amount={amount}
-          currencyOptions={options}
-          onCurrencyChange={(currency)=>setFrom(currency)}
-          onAmountChange={(amount)=>setAmount(amount)}
-          selectedCurrency={from}
-          
-          />
-        </div>
-        <div className="relative w-full h-0.5">
-          <button className="absolute">Swap</button>
-        </div>
-        
+        <div className="w-full">
+          <div className="w-full max-w-md mx-auto border border-gray-50 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                convert()
+              }}
+            >
+              <div className="w-full mb-1">
+                <InputBox
+                  label={from}
+                  amount={amount}
+                  currencyOptions={options}
+                  onCurrencyChange={(currency) => setFrom(currency)}
+                  onAmountChange={(amount) => setAmount(amount)}
+                  selectedCurrency={from}
+                />
+              </div>
+              <div className="relative w-full h-0.5">
+                <button
+                  className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                  onClick={swap}
+                >
+                  Swap
+                </button>
+              </div>
+              <div className="w-full mb-1">
+                <InputBox
+                  label={to}
+                  amount={convertedAmount}
+                  currencyOptions={options}
+                  onCurrencyChange={(currency) => setTo(currency)}
+                  onAmountChange={(amount) => setAmount(amount)}
+                  selectedCurrency={to}
+                  amountDisabled
+                />
+                <button type="submit" className="w-full px-4 py-3 text-white bg-blue-600 rounded-lg">Convert</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
