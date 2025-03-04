93329c581f7a376a87bc54c72fc61621909cfaa8
Mohammad Rafi Safidrawan <safidrawan@gmail.com>
2025-03-04 18:35:10 +0430

    Login and userContext are made

--- useContext/src/components/Login.jsx
+++ useContext/src/components/Login.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import UserContext from "../Context/UserContext";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {setUser} = useContext(UserContext)
+
+  const handleSubmit = (e) =>{
+    // Prevent default
+  }
+
   return (
-    <div>Login</div>
-  )
+    <div>
+      <h2>Login</h2>
+      <input 
+      type="text"
+      value={username}
+      onChange={(e)=>setUsername(e.target.value)}
+      placeholder="Username"
+      />
+      {' '}
+      <input type="password" 
+      value={password}
+      onChange={(e)=>setPassword(e.target.value)}
+      placeholder="Password"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
---
+++ useContext/src/components/Profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div className='text-3xl my-3 text-center'>Profile: {}</div>
+  )
+}
+
+export default Profile
