1539228b9558b664e3a60585abcedbf2b12f68c4
Mohammad Rafi Safidrawan <safidrawan@gmail.com>
2025-03-14 22:35:02 +0430

    some name changes and auth service added

---
+++ blog-app/src/conf/conf.js
@@ -0,0 +1,7 @@
+const conf = {
+    appwriteUrl: String(process.env.REACT_APP_APPWRITE_URL),
+    appwriteProjectId: String(process.env.REACT_APP_APPWRITE_PROJECT_ID),
+    appwriteCollectionId: String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
+    appwriteDatabaseId: String(process.env.REACT_APP_APPWRITE_DATABASE_ID),
+    appwriteBuketId: String(process.env.REACT_APP_APPWRITE_BUCKET_ID),
+}
