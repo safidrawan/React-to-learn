2316e971d558ce41d615f0c98c8cf94478c6f2d8
Mohammad Rafi Safidrawan <safidrawan@gmail.com>
2025-03-28 12:17:11 +0430

    blog removed

--- blog-app/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function App() {
-  return (
-    <div className='text-3xl'>App</div>
-  )
-}
-
-export default App
--- blog-app/src/conf/conf.js
+++
@@ -1,7 +0,0 @@
-const conf = {
-    appwriteUrl: String(process.env.REACT_APP_APPWRITE_URL),
-    appwriteProjectId: String(process.env.REACT_APP_APPWRITE_PROJECT_ID),
-    appwriteCollectionId: String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
-    appwriteDatabaseId: String(process.env.REACT_APP_APPWRITE_DATABASE_ID),
-    appwriteBucketId: String(process.env.REACT_APP_APPWRITE_BUCKET_ID),
-}
